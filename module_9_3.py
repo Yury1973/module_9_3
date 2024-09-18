742308102fddbe51f9bde634df69f084b6563ade
Yury1973 <abashkinyn@gmail.com>
2024-09-18 20:54:32 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,8 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[x]) == len(second[x]) for x in range(0, len(first)))
+
+print(list(first_result))
+print(list(second_result))
